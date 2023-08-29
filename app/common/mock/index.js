ce78be7c395f6e2b3244083fa7cdbabc0b02c07d
coderzbar <coderzbar@gmail.com>
2023-08-29 15:32:08 +0530

    header remove error

---
+++ app/common/mock/index.js
@@ -0,0 +1,22 @@
+export const HeaderData = [
+  {
+    id: 1,
+    slug: "/",
+    name: "home",
+  },
+  {
+    id: 2,
+    slug: "/about",
+    name: "about",
+  },
+  {
+    id: 3,
+    slug: "/projects",
+    name: "projects",
+  },
+  {
+    id: 4,
+    slug: "/blogs",
+    name: "blogs",
+  },
+];
--- app/layout/Header/index.jsx
+++ app/layout/Header/index.jsx
@@ -1,42 +1,39 @@
+import { HeaderData } from "app/common/mock";
 import React, { useState } from "react";
 
 export const Header = ({ styles }) => {
   const [open, setOpen] = useState(false);
   return (
     <>
-      <nav className={styles.nav_wrapper}>
-        <p className={styles.logo}>Logo</p>
-        <div class={`${styles.primary_nav} ${open ? styles.active : ""}`}>
-          <ul class={`${styles.list} ${open ? styles.nav_opac : ""}`}>
-            <li>
-              <a class={`${styles.nav_link}`} href="#">
-                Home
-              </a>
-            </li>
-            <li>
-              <a class={`${styles.nav_link}`} href="#">
-                About
-              </a>
-            </li>
-            <li>
-              <a class={`${styles.nav_link}`} href="#">
-                Blog
-              </a>
-            </li>
-            <li>
-              <a class={`${styles.nav_link}`} href="#">
-                Contact
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div
-          onClick={() => setOpen(!open)}
-          className={`${styles.menu} ${open ? styles.opened : ""}`}
-        >
-          <div className={styles.middle}></div>
-        </div>
-      </nav>
+      <div className="container">
+        <nav className={styles.nav_wrapper}>
+          <div className={styles.logo}>Logo</div>
+          <div className="d-flex align-items-center">
+            <div className={styles.contact_btn}>
+              <button>Contact</button>
+            </div>
+            <div
+              onClick={() => setOpen(!open)}
+              className={`${styles.menu} ${open ? styles.opened : ""}`}
+            >
+              <div className={styles.middle}></div>
+            </div>
+          </div>
+        </nav>
+      </div>
+      <div class={`${styles.primary_nav} ${open ? styles.active : ""}`}>
+        <ul class={`${styles.list} ${open ? styles.nav_opac : ""}`}>
+          {HeaderData?.map((el) => {
+            return (
+              <li style={{ visibility: open ? "visible" : "hidden" }}>
+                <a class={`${styles.nav_link}`} href={el?.link}>
+                  {el?.name}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </>
   );
 };
