4291496289c2e570ec357405a34c41c6e2378766
coderzbar <coderzbar@gmail.com>
2023-09-01 13:32:57 +0530

    added footer

--- components/Banner/index.jsx
+++ components/Banner/index.jsx
@@ -5,23 +5,18 @@ import styles from "styles/Banner.module.css";
 export const Banner = () => {
   return (
     <div className={styles.banner_canvas}>
-      {/* <div className={styles.content}>
-        <div className={styles.head}>Hello world</div>
-        <div className={styles.subHead}>
-          I'm a Product Designer working remotely from Mohali, India.
+      <div className="container">
+        <div className={styles.head}>
+          <h1>Hello </h1>
         </div>
-        <div className={styles.body}>
-          <p>
+        <div className={`${styles.body} row justify-content-center`}>
+          <p className="col-xl-6 col-lg-10 text-center">
             Over the past 12+ years, I've worked in various areas of digital
             design, including front-end development, email, marketing, and app
             UI/UX. I'm proud to have worn many hats.
           </p>
-          <p>
-            These days, I focus on building Help Scout's knowledge base and AI
-            tools as a Senior Product Designer.
-          </p>
         </div>
-      </div> */}
+      </div>
     </div>
   );
 };
